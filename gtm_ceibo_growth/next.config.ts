77215ee327194e825dcd2a2decb7c4a30393f7a6
smilladev <smilladev@gmail.com>
2025-04-04 11:46:45 -0300

    fix .env

--- gtm_ceibo_growth/next.config.ts
+++ gtm_ceibo_growth/next.config.ts
@@ -2,20 +2,7 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true, // Ayuda a identificar problemas en desarrollo
-  async redirects() {
-    return [
-      {
-        source: '/api/utm', // Ruta del endpoint UTM
-        destination: 'https://gtm-cg-smilladevs-projects.vercel.app/aaa', // URL de redirección para UTM
-        permanent: false, // Redirección temporal (307)
-      },
-      {
-        source: '/api/gtm', // Ruta del endpoint GTM
-        destination: 'https://gtm-cg-smilladevs-projects.vercel.app/bbb', // URL de redirección para GTM
-        permanent: false, // Redirección temporal (307)
-      },
-    ];
-  },
+  
 };
 
 export default nextConfig;
