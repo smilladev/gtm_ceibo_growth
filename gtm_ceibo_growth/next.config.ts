0a4d5cb42af3c5d40a2e56f792f0fc40a8d1b309
smilladev <smilladev@gmail.com>
2025-04-04 11:12:06 -0300

    Add config

--- gtm_ceibo_growth/next.config.ts
+++ gtm_ceibo_growth/next.config.ts
@@ -1,7 +1,22 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Ayuda a identificar problemas en desarrollo
+  swcMinify: true, // Optimiza el rendimiento en producción
+  async redirects() {
+    return [
+      {
+        source: '/api/utm', // Ruta del endpoint UTM
+        destination: 'https://gtm-cg-smilladevs-projects.vercel.app/aaa', // URL de redirección para UTM
+        permanent: false, // Redirección temporal (307)
+      },
+      {
+        source: '/api/gtm', // Ruta del endpoint GTM
+        destination: 'https://gtm-cg-smilladevs-projects.vercel.app/bbb', // URL de redirección para GTM
+        permanent: false, // Redirección temporal (307)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
